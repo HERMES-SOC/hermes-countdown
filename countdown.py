92b90a1f506481a67097b82a3c9bd589c6b8bf4d
Steven Christe <ehsteve@users.noreply.github.com>
2022-03-01 10:40:56 -0500

    update on launch date

--- countdown.py
+++ countdown.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 
-LAUNCH_DATE = datetime(2024,11,1)
+LAUNCH_DATE = datetime(2024,11,24)
 
 days_to_launch = LAUNCH_DATE - datetime.now()
 
@@ -9,4 +9,5 @@ print(days_to_launch.days)
 
 with open('README.md', "w") as f:
     f.write("# HERMES Launch Countdown Clock\n\n")
+    f.write("Launch date:{0}\n".format(LAUNCH_DATE))
     f.write("# {0} days".format(days_to_launch.days))
