3e58b47e7e47d97c6b9fa9e53ba0ebabec1f32b3
Steven Christe <ehsteve@users.noreply.github.com>
2022-03-12 10:19:56 -0500

    bug fix

--- countdown.py
+++ countdown.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from datetime import datetime
 
 LAUNCH_DATE = datetime(2024,11,24)
@@ -9,4 +8,4 @@ with open('README.md', "w") as f:
     f.write("# HERMES Launch Countdown Clock\n\n")
     f.write("Launch date: {:%A, %B %d %Y}\n".format(LAUNCH_DATE))
     f.write("# {0} days".format(days_to_launch.days))
-    f.write("\n\nLast Updated {:%A, %B %d %Y}".format(datetime.now())
+    f.write("\n\nLast Updated {:%A, %B %d %Y}".format(datetime.now()))
