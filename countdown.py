cd1ecea26a10f2fccaaaf73803260c87110dcc7e
Steven Christe <ehsteve@users.noreply.github.com>
2022-07-21 13:31:31 -0400

    Update countdown.py

--- countdown.py
+++ countdown.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
-LAUNCH_DATE = datetime(2024,11,24)
+LAUNCH_DATE = datetime(2025,10,01)
 
 days_to_launch = LAUNCH_DATE - datetime.now()
 
 with open('README.md', "w") as f:
     f.write("# HERMES Launch Countdown Clock\n\n")
-    f.write("Launch date (No Earlier Than): {:%A, %B %d %Y}\n".format(LAUNCH_DATE))
+    f.write("Launch date (???): {:%A, %B %d %Y}\n".format(LAUNCH_DATE))
     f.write("# {0} days".format(days_to_launch.days))
     f.write("\n\nLast Updated {:%A, %B %d %Y}".format(datetime.now()))
